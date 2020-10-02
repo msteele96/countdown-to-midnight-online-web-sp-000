27311ab1d4590b38bbe11fc069e4ddfc489b6b27
Marc Steele <marcsteele2014@gmail.com>
2020-10-02 22:54:31 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,22 @@
-#write your code here
+def countdown(integer)
+  loop do
+    puts "#{integer} SECOND(S)!"
+    integer -= 1
+    if integer == 0
+      return "HAPPY NEW YEAR!"
+      break
+    end
+  end
+end
 
-def countdown
+def countdown_with_sleep(integer)
+  loop do
+    puts "#{integer} SECOND(S)!"
+    sleep(1)
+    integer -= 1
+    if integer == 0
+      return "HAPPY NEW YEAR!"
+      break
+    end
+  end
 end
